6d264bc51a40dffb97557b152401edf496201f70
danielmcquillen <daniel@mcquilleninteractive.com>
2017-02-28 16:55:16 +1100

    Trying to get checkbox to be required. Bah.

--- ibio_custom_reg_form/forms.py
+++ ibio_custom_reg_form/forms.py
@@ -1,5 +1,6 @@
 from .models import ExtraInfo
 from django.forms import ModelForm
+from django import forms
 
 class ExtraInfoForm(ModelForm):
     """
@@ -11,6 +12,8 @@ class ExtraInfoForm(ModelForm):
             "required": u"I agree to participate in the iBiology Courses study.",
             "invalid": u"You must agree in order to participate in iBiology Courses.",
         }
+
+    user_agreement_ibio_study = forms.BooleanField(required=True)
 
     class Meta(object):
         model = ExtraInfo
